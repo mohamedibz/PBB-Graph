b47d5e1dbccb11546afe4182bfdc94a0994d068c
moha11 <mohamed.tanger2001@gmail.com>
2025-02-12 18:00:05 +0100

    Adaptación de los esquemas a los cambios de los contratos solidity

--- src/factory.ts
+++ src/factory.ts
@@ -1,6 +1,11 @@
 import { DataSourceTemplate } from "@graphprotocol/graph-ts"
-import { PBBCreated as PBBCreatedEvent } from "../generated/PBBFactory/PBBFactory"
 import { PBB, User } from "../generated/schema"
+import { 
+        PBBCreated as PBBCreatedEvent, 
+        ImplementationAdded as ImplementationAddedEvent,
+        PBBUpdated as PBBUpdatedEvent 
+        } from "../generated/PBBFactory/PBBFactory"
+
 
 export function handlePBBCreated(event: PBBCreatedEvent): void {
   let pbb = new PBB(event.params.pbbAddress.toHex());
@@ -14,10 +19,40 @@ export function handlePBBCreated(event: PBBCreatedEvent): void {
 
   pbb.creator = user.id;
   pbb.name = event.params.name;
+  pbb.admins = [user.id];
   pbb.timestamp = event.block.timestamp;
-  pbb.authorizedUsers = [];  // Inicializa el array vacío para evitar el error
-  pbb.messages = [];         // Inicializa como array vacío
+  pbb.members = [];  // Inicializa el array vacío para evitar el error
+  pbb.messages = [];
+  pbb.version = event.params.version;         // Inicializa como array vacío
   pbb.save();
 
   DataSourceTemplate.create("PublicBulletinBoard", [event.params.pbbAddress.toHex()]);
+}
+
+export function handleImplementationAdded(event: ImplementationAddedEvent): void {
+  // Usamos la versión como identificador único (si cada versión es única).
+  /*
+  let id = event.params.version.toString();
+  let impl = new ImplementationAdded(id);
+  
+  impl.implementation = event.params.implementation.toHex();
+  impl.version = event.params.version;
+  impl.timestamp = event.block.timestamp;
+  
+  impl.save();
+  */
+}
+
+export function handlePBBUpdated(event: PBBUpdatedEvent): void {
+  // Cargamos la entidad PBB usando la dirección del PBB
+  let pbb = PBB.load(event.params.pbbAddress.toHex());
+  
+  if (pbb == null) {
+    // Si no se encuentra la entidad, se puede omitir o loguear la situación
+    return;
+  }
+  
+  // Actualizamos la versión del PBB
+  pbb.version = event.params.version;
+  pbb.save();
 }
--- src/pbb.ts
+++ src/pbb.ts
@@ -1,6 +1,64 @@
 import { Message, PBB, User } from '../generated/schema';
-import { UserAuthorized as UserAuthorizedEvent, MessageAdded as MessageAddedEvent, UserRevoked as UserRevokedEvent} from '../generated/templates/PublicBulletinBoard/PBB';
+import { 
+        MemberAdded as MemberAddedEvent,
+        MemberRemoved as MemberRemovedEvent,
+        MessageAdded as MessageAddedEvent,
+        AdminAdded as AdminAddedEvent,
+        AdminRevoked as AdminRevokedEvent,
+        } from '../generated/templates/PublicBulletinBoard/PBB';
 
+
+export function handleMemberAdded(event: MemberAddedEvent): void {
+
+  let user = User.load(event.params.newMember.toHex());
+  if (!user) {
+    user = new User(event.params.newMember.toHex());
+    user.authorizedPBBs = new Array<string>();  // Inicializa el arreglo de PBBs autorizados
+  }
+
+  let pbb = PBB.load(event.address.toHex());
+  if (pbb) {
+    // Añadir el PBB a la lista de PBBs autorizados del usuario
+    if (!user.authorizedPBBs) {
+      user.authorizedPBBs = new Array<string>();
+    }
+    if (!user.authorizedPBBs!.includes(pbb.id)) {
+      user.authorizedPBBs = user.authorizedPBBs!.concat([pbb.id]);
+    }
+
+    // Añadir el usuario a la lista de usuarios autorizados del PBB
+    if (!pbb.members) {
+      pbb.members = new Array<string>();
+    }
+    if (!pbb.members.includes(user.id)) {
+      pbb.members = pbb.members.concat([user.id]);
+    }
+
+    // Guardar el usuario y el PBB actualizados
+    user.save();
+    pbb.save();
+  }
+
+}
+
+export function handleMemberRemoved(event: MemberRemovedEvent): void {
+
+  let pbb = PBB.load(event.address.toHex());
+  if (pbb) {
+    if (!pbb.members) {
+      pbb.members = [];  // Inicializa si es null o undefined
+    }
+
+    let index = pbb.members.indexOf(event.params.revokedMember.toHex());
+    if (index > -1) {
+      let updatedUsers = pbb.members.slice(0); // Crea una copia para evitar problemas al modificar directamente
+      updatedUsers.splice(index, 1);
+      pbb.members = updatedUsers;
+      pbb.save();
+    }
+  }
+
+}
 
 export function handleMessageAdded(event: MessageAddedEvent): void {
 
@@ -25,6 +83,101 @@ export function handleMessageAdded(event: MessageAddedEvent): void {
   message.topic = event.params.topic;
   message.content = event.params.content;
   message.timestamp = event.block.timestamp;
+  message.txHash = event.transaction.hash;
+  message.save();
+
+  // Asociar el mensaje a la PBB
+  let messages = pbb.messages || []; 
+  messages.push(message.id);
+  pbb.messages = messages;
+  pbb.save();  
+
+}
+
+export function handleAdminAdded(event: AdminAddedEvent): void {
+
+  let user = User.load(event.params.newAdmin.toHex());
+  if (!user) {
+    user = new User(event.params.newAdmin.toHex());
+    user.authorizedPBBs = new Array<string>();  // Inicializa el arreglo de PBBs autorizados
+  }
+
+  let pbb = PBB.load(event.address.toHex());
+  if (pbb) {
+
+    /*
+    // Añadir el PBB a la lista de PBBs autorizados del usuario
+    if (!user.authorizedPBBs) {
+      user.authorizedPBBs = new Array<string>();
+    }
+    if (!user.authorizedPBBs!.includes(pbb.id)) {
+      user.authorizedPBBs = user.authorizedPBBs!.concat([pbb.id]);
+    }
+    */
+
+    // Añadir el usuario a la lista de usuarios autorizados del PBB
+    if (!pbb.admins) {
+      pbb.admins = new Array<string>();
+    }
+    if (!pbb.members.includes(user.id)) {
+      pbb.admins = pbb.admins.concat([user.id]);
+    }
+
+    // Guardar el usuario y el PBB actualizados
+    user.save();
+    pbb.save();
+  }
+
+}
+
+export function handleAdminRevoked(event: AdminRevokedEvent): void {
+
+  let pbb = PBB.load(event.address.toHex());
+  if (pbb) {
+    if (!pbb.members) {
+      pbb.members = [];  // Inicializa si es null o undefined
+    }
+
+    let index = pbb.members.indexOf(event.params.revokedAdmin.toHex());
+    if (index > -1) {
+      let updatedUsers = pbb.members.slice(0); // Crea una copia para evitar problemas al modificar directamente
+      updatedUsers.splice(index, 1);
+      pbb.members = updatedUsers;
+      pbb.save();
+    }
+  }  
+
+}
+
+
+
+
+/*
+
+export function handleMessageAdded(event: MessageAddedEvent): void {
+
+  let message = new Message(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+
+  let pbb = PBB.load(event.address.toHex());
+
+  if (!pbb) {
+    // Si no encuentra la PBB, evita continuar
+    return;
+  }
+
+  message.pbb = pbb.id;
+
+  let user = User.load(event.params.sender.toHex());
+  if (!user) {
+    user = new User(event.params.sender.toHex());
+    user.save();
+  }
+  
+  message.sender = user.id;
+  message.topic = event.params.topic;
+  message.content = event.params.content;
+  message.timestamp = event.block.timestamp;
+  message.txHash = event.transaction.hash;
   message.save();
 
   // Asociar el mensaje a la PBB
@@ -66,10 +219,6 @@ export function handleUserAuthorized(event: UserAuthorizedEvent): void {
   }
 }
 
-
-
-
-
 export function handleUserRevoked(event: UserRevokedEvent): void {
   let pbb = PBB.load(event.address.toHex());
   if (pbb) {
@@ -87,3 +236,4 @@ export function handleUserRevoked(event: UserRevokedEvent): void {
   }
 }
 
+*/
